b52cc7440c4ed5c9ccc0de31b4874ac061e0e448
Shubha213 <94108382+Shubha213@users.noreply.github.com>
2021-11-14 15:12:50 +0530

    Add files via upload

---
+++ experiment.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+ int *ptr_1;
+ int *ptr_2;
+ int *ptr_3;
+ 
+ ptr_1 = (int*)malloc(1*sizeof(int));
+ ptr_2 = (char*)malloc(1*sizeof(char)*1);
+ ptr_3 = (float*)malloc(1*sizeof(float));
+
+ printf("")
+
+
+return 0;
+}
